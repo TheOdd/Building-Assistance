332f1bc64b2cd34a512a22c0895e8e311fbc8795
TheOdd <owenhines07@gmail.com>
2018-11-26 14:42:00 -0600

    Changed some code style details

--- src/main/java/me/owenhines/buildingassistance/BuildingAssistance.java
+++ src/main/java/me/owenhines/buildingassistance/BuildingAssistance.java
@@ -8,8 +8,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = BuildingAssistance.MODID, name = BuildingAssistance.NAME, version = BuildingAssistance.VERSION)
-public class BuildingAssistance
-{
+public class BuildingAssistance {
     public static final String MODID = "buildingassistance";
     public static final String NAME = "Building Assistance";
     public static final String VERSION = "1.0";
@@ -17,14 +16,12 @@ public class BuildingAssistance
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
+    public void preInit(FMLPreInitializationEvent event) {
+    	logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         logger.info("Building Assistance Loaded.");
     }
 }
